d58cf21475e7a44931fa66cfb1515fe8a0aec56e
nokutomi <nathankutomi@hotmail.com>
2022-06-04 19:02:16 -0300

    feat: week7 assignment 2

--- chamber/js/visits.js
+++ chamber/js/visits.js
@@ -1,6 +1,8 @@
 let currDate = Date.now();
 const visitsDisplay = document.querySelector(".visits");
 
+const oneDayInMiliseconds = 24 * 60 * 60 * 1000;
+
 const zeroHours = (dateNum) => {
   let date = new Date(dateNum);
   let year = date.getFullYear();
@@ -9,8 +11,17 @@ const zeroHours = (dateNum) => {
   return new Date(year, month, day);
 };
 
-// localStorage.setItem("previous-visit", 1653447600000);
 // localStorage.setItem("last-visit", 1653879600000);
+
+if (!window.localStorage.getItem("previous-visit")) {
+  localStorage.setItem(
+    "previous-visit",
+    Number(zeroHours(currDate) - oneDayInMiliseconds)
+  );
+}
+if (!window.localStorage.getItem("last-visit")) {
+  localStorage.setItem("last-visit", Number(zeroHours(currDate)));
+}
 
 const previousVisit = Number(window.localStorage.getItem("previous-visit"));
 const lastVisit = Number(window.localStorage.getItem("last-visit"));
@@ -26,27 +37,26 @@ let lastDay = new Date(lastVisit);
 // console.log(Number(currDate));
 let days = currDate - lastDay;
 // console.log(days);
-const oneDayInMiliseconds = 24 * 60 * 60 * 1000;
 // console.log(oneDayInMiliseconds);
 
 // console.log(days > oneDayInMiliseconds)
 
 if (days > oneDayInMiliseconds) {
-    // console.log("mais de 1 dia");
-    localStorage.setItem("previous-visit", Number(lastDay));
-    localStorage.setItem("last-visit", Number(zeroHours(currDate)));
-    
-    let daysText = Math.floor(days / oneDayInMiliseconds);
-    // console.log(daysText);
-    
-    visitsDisplay.textContent = daysText;
+  // console.log("mais de 1 dia");
+  localStorage.setItem("previous-visit", Number(lastDay));
+  localStorage.setItem("last-visit", Number(zeroHours(currDate)));
+
+  let daysText = Math.floor(days / oneDayInMiliseconds);
+  // console.log(daysText);
+
+  visitsDisplay.textContent = daysText;
 } else {
-    // console.log("menos de 1 dia");
-    let daysText = Number((lastDay - prevDay) / oneDayInMiliseconds);
-    // //     console.log(lastDay);
-    // //     console.log(prevDay);
-    console.log(daysText);
-    visitsDisplay.textContent = daysText;
+  // console.log("menos de 1 dia");
+  let daysText = Number((lastDay - prevDay) / oneDayInMiliseconds);
+  // //     console.log(lastDay);
+  // //     console.log(prevDay);
+  console.log(daysText);
+  visitsDisplay.textContent = daysText;
 }
 /*
-*/
+ */
